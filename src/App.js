8cc578850f2cdbd453e8c2829a65d009af9f0be3
katering08 <km_osortop@unicah.edu>
2021-02-22 21:53:30 -0600

    For Github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Cards from './components/cards/Cards';
+import CardHolder from './components/cards/CardHolder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Hola Mundo!!!</h1>
+      <CardHolder>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+      </CardHolder>
+    </section>
   );
 }
 
---
+++ src/components/cards/Cards.js
@@ -0,0 +1,17 @@
+function Cards (){
+    //JSX
+    return (
+        <section className="Cards">
+            <h2>Tittle</h2>
+            <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+            </p>
+            <div>
+                <button>Accion A</button>
+                <button>Accion B</button>
+            </div>
+        </section>
+    );
+}
+
+export default Cards;
